1cc16453efc1b43819b9e02e1cf6beb27e02f1ea
Laurent Tréguier <laurent@treguier.org>
2021-03-09 21:57:56 +0100

    Add badge on update available

--- Shared/Components/NavigationBase.cs
+++ Shared/Components/NavigationBase.cs
@@ -1,3 +1,6 @@
+#nullable enable
+
+using System;
 using Microsoft.AspNetCore.Components;
 
 namespace Ynferno.Shared.Components
@@ -5,7 +8,7 @@ namespace Ynferno.Shared.Components
     public abstract class NavigationBase : ComponentBase
     {
         [Parameter]
-        public Destination[] Destinations { get; set; }
+        public Destination[] Destinations { get; set; } = Array.Empty<Destination>();
 
         public string ItemSize => (100 / Destinations.Length) + "%";
     }
@@ -14,6 +17,7 @@ namespace Ynferno.Shared.Components
     {
         public string Route { get; set; }
         public string Text { get; set; }
-        public RenderFragment Icon { get; set; }
+        public RenderFragment? Icon { get; set; }
+        public RenderFragment? Badge { get; set; }
     }
 }
